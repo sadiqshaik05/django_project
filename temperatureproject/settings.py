cf3b5eea76db77c1a8911038eee99aee332ac2d4
sadiqshaik05 <shaiksadiq7525@gmail.com>
2026-02-04 06:58:09 +0530

    Allow all hosts in settings

--- temperatureproject/settings.py
+++ temperatureproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4kqay5j+lu$f%7gru6kbqbi5)e%vbly^vkr8zckl7n8vio@5o*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
